09428282c597aa9bdbab6727e5f54d489566f7ad
Armapillow <bozicr@protonmail.com>
2022-03-29 08:29:07 +0200

    Klasa Texture2D sada podrzava rad sa cubemaps

--- include/rg/Texture2D.h
+++ include/rg/Texture2D.h
@@ -17,6 +17,32 @@
 class Texture2D {
 public:
 
+    Texture(const std::vector<std::string> faces, GLint textureNum)
+    {
+        m_textureNumber = textureNum;
+        glGenTextures(1, &m_textureId);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, m_textureId);
+
+        int width, height, nrComponents;
+        for (unsigned int i = 0; i < faces.size(); i++) {
+            unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
+            if (data) {
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+                stbi_image_free(data);
+            }
+            else {
+                std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+                stbi_image_free(data);
+            }
+        }
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    }
+
+
     Texture2D(const char *pathToTexture, GLint textureNum)
     {
         m_textureNumber = textureNum;
